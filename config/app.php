0fa07897724a910543e9727dfe7f561be6379fea
lzw <lzwmt118125@gmail.com>
2019-05-28 19:32:03 +0000

    fix heroku 500

--- config/app.php
+++ config/app.php
@@ -34,12 +34,12 @@ return [
     |--------------------------------------------------------------------------
     |
     | When your application is in debug mode, detailed error messages with
-    | stack traces will be shown on every error that occurs within your
+    | stack traces will be shown on every error that ocscurs within your
     | application. If disabled, a simple generic error page is shown.
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
